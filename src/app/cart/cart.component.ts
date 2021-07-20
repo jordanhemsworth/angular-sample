4464e50fdb00304134aed9711e41456e7c6d1e26
jordanhemsworth <jordanhemsworth@gmail.com>
2021-07-20 18:12:38 +0100

    Using Forms

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items = this.cartService.getItems();
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {}
+}
